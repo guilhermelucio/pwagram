48d94639932264f958d39e9274b54faace6f9916
Guilherme Lucio <guilhermelucio.design@gmail.com>
2017-12-26 14:29:40 +0000

    Adding the fetch and promise fallbacks

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,9 @@
+let deferredPrompt;
+
+if(!window.Promise) {
+    window.Promise = Promise;
+}
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
@@ -5,3 +11,11 @@ if ('serviceWorker' in navigator) {
             console.log('Service Worked registered!!!');
         });
 }
+
+// Right before displaying the add to home banner
+window.addEventListener('beforeinstallprompt', event => {
+    console.log('beforeinstallprompt');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
---
+++ public/sw.js
@@ -0,0 +1,21 @@
+/* 
+ * `self` refers to the serviceWorker
+ * NOTE - The service worker does not have access to the DOM
+ */
+
+// Lifecycle event
+self.addEventListener('install',  event => (
+    console.log('[Service Worker] Installing service worker...', event)
+));
+
+// Lifecycle event
+self.addEventListener('activate', event => (
+    console.log('[Service Worker] Activating service worker...', event)
+));
+
+self.addEventListener('fetch', event => {
+    console.log('[Service Worker] Fetching something...', event);
+
+    // A service worker can be used as a kind of proxy
+    event.respondWith(fetch(event.request));
+});
