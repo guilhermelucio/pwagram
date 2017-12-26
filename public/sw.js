0ee9ddfe47fd424654e34e3fb4d80f8f75f16432
Guilherme Lucio <guilhermelucio.design@gmail.com>
2017-12-26 14:52:59 +0000

    Preparing for starting the cache section

--- public/sw.js
+++ public/sw.js
@@ -4,17 +4,17 @@
  */
 
 // Lifecycle event
-self.addEventListener('install',  event => (
-    console.log('[Service Worker] Installing service worker...', event)
-));
+// self.addEventListener('install',  event => (
+//     console.log('[Service Worker] Installing service worker...', event)
+// ));
 
 // Lifecycle event
-self.addEventListener('activate', event => (
-    console.log('[Service Worker] Activating service worker...', event)
-));
+// self.addEventListener('activate', event => (
+//     console.log('[Service Worker] Activating service worker...', event)
+// ));
 
 self.addEventListener('fetch', event => {
-    console.log('[Service Worker] Fetching something...', event);
+    // console.log('[Service Worker] Fetching something...', event);
 
     // A service worker can be used as a kind of proxy
     event.respondWith(fetch(event.request));
