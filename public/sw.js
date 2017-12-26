8fb89fd0f325de607c2b3657a81dc9300c6b05db
Guilherme Lucio <guilhermelucio.design@gmail.com>
2017-12-26 16:31:38 +0000

    Caching basic dynamic file requests

--- public/sw.js
+++ public/sw.js
@@ -14,6 +14,9 @@ self.addEventListener('install',  event => {
         caches.open('pwagram-static')
             .then(cache => {
                 console.log('[Service Worker] Precaching App Shell');
+                /**
+                 * add takes a request, fires and store the result in the cache
+                 */
                 cache.addAll([
                     '/',
                     '/index.html',
@@ -33,13 +36,31 @@ self.addEventListener('install',  event => {
 
 // Lifecycle event
 self.addEventListener('activate', event => {
-    console.log('[Service Worker] Activating service worker...', event);
+    // console.log('[Service Worker] Activating service worker...', event);
     return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
     // console.log('[Service Worker] Fetching something...', event);
     // A service worker can be used as a kind of proxy
+
+    const cacheDynamicFiles = (response) => {
+        /**
+         * `put` does do any request, just stores data, different than `add`
+         */
+        return caches.open('pwagram-dynamic')
+            .then(cache => {
+                /**
+                 * GOTCHA, response can just be used once, it sounds odd but anyway...
+                 * if response is used on the `put` method, the value returned by the
+                 * method that also uses response will be `undefined`. Thus in order
+                 * to store the value and return it, so that the HTML can render it,
+                 * the `cache` put method receives a clone of the response
+                 */
+                cache.put(event.request.url, response.clone());
+                return response;
+            });
+    }
 
     /**
      * caches.match will look for keys on the application cache,
@@ -49,7 +70,7 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                return (response) ? response : fetch(event.request)
+                return (response) ? response : fetch(event.request).then(cacheDynamicFiles)
             })
     );
 });
